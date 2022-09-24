dc9fb4a74b9f3e007192c433b827c3372f3947ac
femmyte <sanyaoluadefemi@gmail.com>
2022-09-24 07:55:02 +0100

    article cms

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -92,7 +92,7 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO:Done Acquire a token from a built msal app, along with the appropriate redirect URI
+        # TODO:Don Acquire a token from a built msal app, along with the appropriate redirect URI
         # result = None
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
